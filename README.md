99fa8e9a770355b0652b4588d47d747769371571
Johann Freitas <johannmarques461@gmail.com>
2023-08-13 17:11:44 -0300

    updating readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # wordcloud
 Creating a wordcloud displaying my interests for my README.md
+
+I consider myself a Master-of-none. So I felt plotting a wordcloud would be the most efficient way to represent my
+preferences, attributing weights $\omega_i$ to each topic $i$.
+
+I use [wordcloud](http://amueller.github.io/word_cloud/) library to generate the plot.
--- main.py
+++ main.py
@@ -4,21 +4,33 @@ from PIL import Image
 import numpy as np
 import random
 
+# Creating a color function
 def purple_color_func(word, font_size, position, orientation, random_state=None,
                     **kwargs):
     return "hsl(264, 100%%, %d%%)" % random.randint(25, 65)
 
+# Words list
 words = ['Econometrics', 'Time Series', 'Forecasting',
          'Macroeconomics', 'Finance', 'Industrial Organization',
          'Soccer', 'Elastic Net', 'Data Viz', 'Urban Economics',
          'Infrastructure', 'Applied Micro', 'Risk',
          'Causal Inference', 'Development', 'Brazil',
          'Energy', 'Python', 'R', 'Public Transport']
+
+# And the respective weights
 weights = [50, 10, 20, 10, 10, 10, 15, 30, 20, 15, 10, 5, 1, 1, 1,
            15, 20, 20, 50, 5]
 
+# Checking whether words and weights have the same length
+if len(words) == len(weights) :
+    print('Both vectors have same length. Everything is ok :)')
+else :
+    print('Ooops! words has {} entries while weights has {}'.
+          format(len(words), len(weights)))
+
 freq = {words[ii] : weights[ii] for ii in range(len(words))}
 
+# Image for mask
 gato = np.array(Image.open('gato.png'))
 
 # Generate the cloud
