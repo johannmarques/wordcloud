d601628c7590cec924fb4ec1116bd3aa02a5b4bc
Johann Freitas <johannmarques461@gmail.com>
2023-08-13 16:57:25 -0300

    Wordcloud

---
+++ main.py
@@ -0,0 +1,35 @@
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+import random
+
+def purple_color_func(word, font_size, position, orientation, random_state=None,
+                    **kwargs):
+    return "hsl(264, 100%%, %d%%)" % random.randint(25, 65)
+
+words = ['Econometrics', 'Time Series', 'Forecasting',
+         'Macroeconomics', 'Finance', 'Industrial Organization',
+         'Soccer', 'Elastic Net', 'Data Viz', 'Urban Economics',
+         'Infrastructure', 'Applied Micro', 'Risk',
+         'Causal Inference', 'Development', 'Brazil',
+         'Energy', 'Python', 'R', 'Public Transport']
+weights = [50, 10, 20, 10, 10, 10, 15, 30, 20, 15, 10, 5, 1, 1, 1,
+           15, 20, 20, 50, 5]
+
+freq = {words[ii] : weights[ii] for ii in range(len(words))}
+
+gato = np.array(Image.open('gato.png'))
+
+# Generate the cloud
+
+wc = WordCloud(background_color="rgba(255, 255, 255, 0)", mode="RGBA",
+               mask = gato, random_state = 3)
+wc.generate_from_frequencies(freq)
+
+# Display the generated image:
+plt.imshow(wc.recolor(color_func=purple_color_func, random_state=3),
+           interpolation="bilinear")
+plt.axis("off")
+plt.show()
+wc.to_file('wordcloud.png')
